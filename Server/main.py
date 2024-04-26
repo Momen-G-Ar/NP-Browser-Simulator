90d547a09bf2b7756db185e156acbf6fe7958666
Momen-G-Ar <cp.momen@gmail.com>
2024-04-26 21:29:34 +0300

    complete server

--- Server/main.py
+++ Server/main.py
@@ -1,39 +1,70 @@
 from fastapi import FastAPI, Depends, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
+from typing import List
+import strings
 
 app = FastAPI()
 
-# item:
-# name: str
-# price: number
-#
 security = HTTPBasic()
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-
-
-items = [
+items: List[dict] = [
     {
         'name': 'ice-cream',
         'price': 23.3,
     }
 ]
 
+users = [
+    {
+        'username': 'momin',
+        'password': '12345',
+    },
+    {
+        'username': 'samir',
+        'password': '1122334455',
+    }
+]
 
-@app.get("/items", status_code=status.HTTP_200_OK)
+
+def check_credentials(credentials):
+    return {'username': credentials.username,
+            'password': credentials.password} in users
+
+
+@app.get("/items", status_code=status.HTTP_200_OK, response_class=HTMLResponse)
 async def get_items(credentials: HTTPBasicCredentials = Depends(security)):
-    if credentials.username == 'momin':
-        return {"message": 'OK1', 'items': items, 'length': len(items)}
-    return {"message": 'OK2', 'items': items, credentials: credentials, 'length': len(items)}
+    if not check_credentials(credentials):
+        return strings.invalid_credentials_html
+    html = strings.start_html + strings.our_items
+    for index, _item in enumerate(items):
+        html += strings.item.format(_item['name'], _item['price'])
+    html += strings.end_html
+    return html
 
 
-@app.post("/item")
-async def create_item(item: Item, credentials: HTTPBasicCredentials = Depends(security)):
+@app.delete("/item", status_code=status.HTTP_204_NO_CONTENT, response_class=HTMLResponse)
+async def delete_items(item: dict, credentials: HTTPBasicCredentials = Depends(security)):
+    if not check_credentials(credentials):
+        return strings.invalid_credentials_html
+    index_to_delete = None
+    for i, item_ in enumerate(items):
+        if item_['name'] == item['name']:
+            index_to_delete = i
+            break
+    html_to_be_send = strings.start_html + strings.end_html
+    # If the item is found, delete it using del
+    if index_to_delete is not None:
+        del items[index_to_delete]
+    return html_to_be_send
+
+
+@app.post("/item", status_code=status.HTTP_201_CREATED, response_class=HTMLResponse)
+async def create_item(item: dict, credentials: HTTPBasicCredentials = Depends(security)):
+    if not check_credentials(credentials):
+        return strings.invalid_credentials_html
     items.append(item)
-    if credentials.username == 'momin':
-        print(1)
-    return {'item': item}
+    html_to_be_send = strings.start_html + \
+        strings.item_added_successfully + strings.end_html
+    return html_to_be_send
